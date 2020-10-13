834d3e006f3ee51d3f999a0b31eae96992befe68
liviulenter <lenterliviu@gmail.com>
2020-10-13 15:59:53 +0200

    Customer and CustomerDao by liviu

--- hibernate/src/main/java/hibernate/customer/Customer.java
+++ hibernate/src/main/java/hibernate/customer/Customer.java
@@ -1,5 +1,91 @@
 package hibernate.customer;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "Customer")
+@Table(name = "customer")
 
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long CNP;
+    private Long CustomerId;
+    private String FirstName;
+    private String LastName;
+    private String Email;
+    private String Username;
+    private String Password;
+
+    public void setCustomerId(Long CustomerId) {
+        this.CustomerId = CustomerId;
+    }
+
+    public Long getCustomerId() {
+        return CustomerId;
+    }
+
+    public Long getCNP() {
+        return CNP;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setCNP(Long CNP) {
+        this.CNP = CNP;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "CNP=" + CNP +
+                ", CustomerId=" + CustomerId +
+                ", FirstName='" + FirstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", Email='" + Email + '\'' +
+                ", Username='" + Username + '\'' +
+                ", Password='" + Password + '\'' +
+                '}';
+    }
 }
--- hibernate/src/main/java/hibernate/customer/CustomerDao.java
+++ hibernate/src/main/java/hibernate/customer/CustomerDao.java
@@ -1,4 +1,103 @@
 package hibernate.customer;
+import hibernate.HibernateUtil;
+
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 
 public class CustomerDao {
+
+
+    public void create(Customer customer) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            transaction = session.beginTransaction();
+
+            session.save(customer);
+
+            transaction.commit();
+
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    public Customer findByUsername(String username) {
+        Customer result = null;
+
+        try (Session session = getSession()) {
+            String findByUsernameHql = "FROM Customer p WHERE p.firstName = :firstName";
+            Query<Customer> query = session.createQuery(findByUsernameHql);
+            query.setParameter("firstName", username);
+
+            List<Customer> foundCustomers = query.getResultList();
+
+            if (foundCustomers.isEmpty()) {
+                return result;
+            } else {
+                result = foundCustomers.get(0);
+            }
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public Customer update(Long CustomerId, Customer CustomerDetails) {
+        Customer result = null;
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            Customer CustomerToBeUpdated = session.find(Customer.class, CustomerId);
+
+            transaction = session.beginTransaction();
+
+            CustomerToBeUpdated.setFirstName(CustomerDetails.getFirstName());
+            CustomerToBeUpdated.setLastName(CustomerDetails.getLastName());
+            CustomerToBeUpdated.setEmail(CustomerDetails.getEmail());
+            CustomerToBeUpdated.setCNP(CustomerDetails.getCNP());
+            CustomerToBeUpdated.setUsername(CustomerDetails.getUsername());
+            CustomerToBeUpdated.setPassword(CustomerDetails.getPassword());
+
+            session.update(CustomerToBeUpdated);
+
+            transaction.commit();
+            result = session.find(Customer.class, CustomerId);
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public void delete(Long CustomerId) {
+        Transaction transaction = null;
+        try (Session session = getSession()) {
+            Customer CustomerToBeDeleted = session.find(Customer.class, CustomerId);
+
+            transaction = session.beginTransaction();
+
+            session.delete(CustomerToBeDeleted);
+
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private Session getSession() {
+        return HibernateUtil.getSessionFactory().openSession();
+    }
+
 }
