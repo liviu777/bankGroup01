9d1b80eff897b870745a25265dcc5059011f5b58
Adrian <stinceladrian@gmail.com>
2020-10-21 13:10:47 +0300

    Revert "Delete HibernateUtil.java"
    This reverts commit f61c3915

---
+++ hibernate/src/main/java/hibernate/HibernateUtil.java
@@ -0,0 +1,45 @@
+package hibernate;
+
+import hibernate.account_type.AccountType;
+import hibernate.bank_account.BankAccount;
+import hibernate.customer.Customer;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+import java.util.Properties;
+
+
+public class HibernateUtil {
+    public static SessionFactory getSessionFactory(){
+        Configuration configuration = createConfig();
+
+        ServiceRegistry serviceRegistry =
+                new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+
+    private static Configuration createConfig(){
+        Configuration configuration = new Configuration();
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, "jdbc:mysql://localhost:3306/bankservice?serverTimezone" +
+                "=UTC");
+        settings.put(Environment.USER, "root");
+        settings.put(Environment.PASS, "Univers2016");
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        //settings.put(Environment.SHOW_SQL, "true");
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        settings.put(Environment.HBM2DDL_AUTO, "update");
+
+        configuration.setProperties(settings);
+        configuration.addAnnotatedClass(AccountType.class);
+        configuration.addAnnotatedClass(Customer.class);
+        configuration.addAnnotatedClass(BankAccount.class);
+
+        return configuration;
+    }
+}
